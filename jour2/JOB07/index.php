78617c5b3e89e5b845e57d3cf52897477ffabedf
Nadir-souifi <nadir.souifi@laplateforme.io>
2025-07-30 10:49:28 +0200

    Update index.php

--- jour2/JOB07/index.php
+++ jour2/JOB07/index.php
@@ -4,10 +4,9 @@ echo "<pre>";
 $hauteur = 5;
 
 for ($i = 1; $i <= $hauteur; $i++) {
-    // Espaces
+    
     echo str_repeat(" ", $hauteur - $i);
 
-    // Étoiles
     echo str_repeat("*", 2 * $i - 1);
 
     echo "\n";
